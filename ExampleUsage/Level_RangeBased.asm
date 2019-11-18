abed60196d4b2a1ae69f9c612291e6ce15644329
GhbSmwc <GhbSmwc@users.noreply.github.com>
2019-11-17 19:49:53 -0500

    Add files via upload

--- ExampleUsage/Level_RangeBased.asm
+++ ExampleUsage/Level_RangeBased.asm
@@ -33,6 +33,9 @@
 ;Cave story, for example, do not carry EXP over to the next level, thus the next bar to
 ;fill up is always empty: https://cavestory.fandom.com/wiki/Game_objects#Experience
 
+;Anyway, feel free to use this code as a template for your hack.
+;The range table is at the bottom.
+
 incsrc "../GraphicalBarDefines/GraphicalBarDefines.asm"
 incsrc "../GraphicalBarDefines/StatusBarSettings.asm"
 
@@ -82,9 +85,9 @@ main:
 	..IntervalFound                                        ;|
 	TXA                                                    ;|\Store index*2 here
 	STA !Scratchram_WhatRange                              ;|/
-	CPX.w #((RangeTableEnd-RangeTable)-4)                  ;|\Check if the range is on the second-last and last value in table
+	CPX.w #((RangeTableEnd-RangeTable)-4)                  ;|\Check if X is not pointing to areas beyond the table
 	BCC ..ValidRange                                       ;|/to avoid having another range as last number being the minimum and max being an invalid number.
-	LDX.w #((RangeTableEnd-RangeTable)-4)                  ;|>Cap the range if last number reached. (will also display 100% bar once this range be full, as all others will 0 out when at these intervals)
+	LDX.w #((RangeTableEnd-RangeTable)-4)                  ;|>Cap the range if last number reached. (will also display 100% bar once this range be full, as all others will 0 out when at these intervals), note that index will increment with fill bar.
 	
 	..ValidRange
 	LDA RangeTable,x                                       ;|\Miminum
@@ -159,20 +162,52 @@ main:
 	endif
 	RTL
 	
-	;These are the range values. Each number is the minimum of the range
-	;and the next number is the maximum. Note that the bar displays
-	;from Min to max-1 except the last range.
+	;These are the range values. Each number in table is the minimum to be in a
+	;particular range (and be less than the next number, exclusively).
+	;
 	;NOTE:
 	;-Make sure the numbers are in increasing order and no duplicates.
 	;-Make sure all your numbers are in between "RangeTable" and
 	; "RangeTableEnd".
 	;-The first number must be zero.
+	;-When used as a boss health bar, you must have the last number
+	; be its maximum HP.
+	;
+	;-If you are using this as a phase-based boss health bar, be aware that as the boss
+	; starts with full HP, this value will equal to the last number in the table.
+	; Since that last number is "separate" and not part of the previous index number
+	; (because all numbers are minimums to that index),  you'll have an additional
+	; index that last for 1 HP.
+	;
+	; This means you have a first phase that is very short, lasting until the boss
+	; takes its first hit.
+	;
+	; To prevent this, simply check if the index number is beyond the second-last item,
+	; in this case, if index_x is >= $04, then cap it to $02. This will cause the
+	; full HP amount to be included in the intended first phase at index_x = $02.
+	; See the comments to the right of the table.
+	;
+	;-When used as a level progression bar, thanks to the above information,
+	; it can be treated as the maximum exp and the last level up in which you can
+	; no longer gain any additional EXP on that new level (this final level have
+	; only have 1 number, not only a minimum to be that level but also the maximum).
+	;
+	;-Because every number is the minimum, to be on a particular index, the bar will
+	; never display 100%, rather up to 99%-ish, since the highest number without advancing
+	; is [NextMinimum - 1], which is 1-less from a full bar. The only time the bar is full
+	; is once the last number in table is reached.
+	;
+	; Also, the index number is 16-bit, because:
+	; -The values in the table occupy 2 bytes, so the index is [Index = ItemNumber*2]
+	; -The loop uses BPL to know if all the items in the table have been checked.
+	;  Causing the index number to be treated as "signed".
+	; -Therefore if each item in the table were 1 byte (8-bit), up to 128 items will work properly
+	; -But the table have 16-bit numbers, so it is cut in half again, up to 64 items are allowed.
+	; -Therefore, you can have 16,384 ($4000) items in table on a 16-bit index and 16-bit table numbers.
+	
+	;Table comments here assumes you're using it as a phase-based boss HP bar.
 	RangeTable:
-	dw 0                                   ;>item number 0 ($00)
-	dw 10                                  ;>item number 1 ($02)
-	dw 30                                  ;>item number 2 ($04)
-	dw 60                                  ;>item number 3 ($06)
-	dw 100                                 ;>item number 4 ($08)
-	dw 150                                 ;>item number 5 ($0A)
-	dw 210                                 ;>item number 6 ($0C)
+	dw 0                   ;>item number 0 (index_x = $00) (quantity: 0-100)
+	dw 101                 ;>item number 1 (index_x = $02) (quantity: 101-199 (supposed to be 101-200, but all numbers, even the last, are minimums), the intended first phase)
+	dw 200                 ;>item number 2 (index_x = $04) (quantity: 200 (max health), the unintended "full health phase")
 	RangeTableEnd:
