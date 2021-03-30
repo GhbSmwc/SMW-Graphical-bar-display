b370c5e2c2ba7aee95fde06f66e6b641c62b7e6d
GhbSmwc <GhbSmwc@users.noreply.github.com>
2021-03-29 20:16:59 -0400

    WIP
    this is crashing the game. Also you cannot have other print commands other than for init and main.

--- CustomSprite/SpriteGraphicalBarTest.asm
+++ CustomSprite/SpriteGraphicalBarTest.asm
@@ -15,7 +15,6 @@ SpriteCode:
 	
 Graphics:
 .InputRatio
-	print "custom sprite graphical bar test" $, pc
 	LDA $14							;\Quantity
 	STA !Scratchram_GraphicalBar_FillByteTbl		;/
 	LDA #$00						;\High byte quantity
@@ -41,5 +40,29 @@ Graphics:
 	LDA #$01							;\Use Level-sprite tileset
 	STA $00								;/
 	JSL !ConvertBarFillAmountToTiles				;>Convert tiles.
+	
+	%GetDrawInfo()	;Y = OAM index, $00 = sprite scrn X pos, $01 = sprite scrn Y pos
+	;Note to self:
+	;Y index SHOULD be a multiple of 4
+	;$0200 = Xpos (bits 0-7), bit 8 is in $0420, but the index for that is each 1 rather than 4.
+	;$0201 = Ypos (8-bit).
+	;$0202 = Tile number
+	;$0203 = Tile Properties (YXPPCCCT)
+	;$0204 to $03FF = repeat of above every 4 bytes.
+	;
+	;We start at $0300 because I have a feeling that $0200-$02FF is used by something else.
+	LDA $00
+	STA $0300|!addr,y
+	LDA $01
+	STA $0301|!addr,y
+	LDA !Scratchram_GraphicalBar_FillByteTbl
+	STA $0302|!addr,y
+	LDA.b #%00110001
+	STA $0303|!addr,y
+	STA $030F|!addr,y
+	LDY #$00
+	LDA #$00
+	JSL $01B7B3|!BankB
+	
 	PLX
 	RTS
