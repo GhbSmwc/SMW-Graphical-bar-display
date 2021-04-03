cf5c1dd7ff89f6fd2604cfab86730ace53670229
GhbSmwc <GhbSmwc@users.noreply.github.com>
2021-04-03 11:55:35 -0400

    some updates
    based on the DKR HUD patch.

--- Patch_DrawSpriteGraphicalBar/DrawOamPatch.asm
+++ Patch_DrawSpriteGraphicalBar/DrawOamPatch.asm
@@ -16,10 +16,26 @@
 ;You'd think this can be converted to just an uberasm tool code, but this is wrong. Uberasm tool code runs in between after transferring OAM
 ;RAM ($0200-$041F and $0420-$049F) to SNES register (the code at $008449 does this) and before calling $7F8000 (clears OAM slots), so
 ;therefore, writing OAM on uberasm tool will get cleared before drawn.
+;Don't touch these unless you know what you're doing
+	;Get defines
+		incsrc "SharedSub_Defines/SubroutineDefs.asm"
+	;Check if the user defined oam slot to use is valid
+		assert !OAMSlot%4 == 0, "Invalid OAM slot number."
+;Macros
+	macro findSlot()
+		?loop:
+			;Output: X = Slot to use
+			CPX #$0200			;\If slot index reaches the end, break (no slots available)
+			BEQ ?break			;/
+			LDA $0201|!base,x		;\If slot unused, use that slot
+			CMP #$F0			;|
+			BEQ ?break			;/
+			INX #4				;\Next slot
+			BRA ?loop			;/
+		?break:
+	endmacro
 
-
-assert !OAMSlot%4 == 0, "Invalid OAM slot number."
-
+;Patch stuff
 if !PatchMode == 0
 	org $00A2E6				;>$00A2E6 is the code that runs at the end of the frame, after ALL sprite tiles are written.
 	autoclean JML DrawGraphicalBar
@@ -33,10 +49,44 @@ endif
 
 if !PatchMode == 0
 	freecode
+	;;;;;;;;;;;;;;;;;;;;
+	;Main code
+	;;;;;;;;;;;;;;;;;;;;
 	DrawGraphicalBar:
 		.MainCode
 			;insert code here.
 		.Restore
 			JSL $028AB1		;>Restore the JSL
 			JML $00A2EA		;>Continue onwards
+			
+	;;;;;;;;;;;;;
+	;Subroutines
+	;;;;;;;;;;;;;
+		FindNFreeOAMSlot:
+			;Input: $04 = Number of slots open to search for
+			;Output: Carry = Set if not enough slots found, Clear if enough slots found
+			PHY
+			LDY #$0000					;>Open slot counter
+			LDX #$0010					;>skip the first four slots
+			.loop:						;>to avoid message box conflicts
+				CPX #$0200				;\If all slots searched, there is not enough
+				BEQ .notEnoughFound			;/open slots being found
+
+				LDA $0201|!base,x			;\If slot used, that isn't empty
+				CMP #$F0				;|
+				BNE ..notFree				;/
+				INY					;>Otherwise if it is unused, count it
+				CPY $04					;\If we find n slots that are free, break
+				BEQ .enoughFound			;/
+				..notFree:
+					INX #4				;\Check another slot
+					BRA .loop			;/
+			.notEnoughFound:
+				SEC
+				BRA .Done
+			.enoughFound:
+				CLC
+			.Done
+				PLY
+				RTS
 endif
