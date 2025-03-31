d1d2cdbc8016298ce769ad3eea9ff785964838bc
? <GhbSmwc@users.noreply.github.com>
2025-03-31 18:31:31 -0400

    ...

--- GraphicalBarDefines/GraphicalBarDefines.asm
+++ GraphicalBarDefines/GraphicalBarDefines.asm
@@ -124,7 +124,7 @@ endif
 		!Setting_GraphicalBar_SNESMathOnly = 0
 			;^Info follows:
 			;-Set this to 0 if your code calls the graphical bar routines under the SA-1 processor on SA-1
-			; ROM. Otherwise set it to 1 if it only calls it under the SNES CPU.
+			; ROM. Otherwise set it to 1 if it only calls them under the SNES CPU.
 			;
 			; As an important note: certain emulators follows a rule that only the correct CPU can access
 			; the registers of the matching type (e.g. SA-1 registers can only be used by SA-1 CPU, not SNES).
